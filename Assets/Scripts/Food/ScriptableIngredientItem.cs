b600fad0985cb04280548005cf382fe5916d6599
SemihDurmus1 <58946672+SemihDurmus1@users.noreply.github.com>
2025-01-16 09:18:43 +0300

    *changed FoodItem => ScriptableIngredientItem
    -FoodItem Renamed to ScriptableIngredientItem
    -IsCompatibleWith func added
    -variables renamed(food names changed to Ingredient)

---
+++ Assets/Scripts/Food/ScriptableIngredientItem.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+namespace Ingredient
+{
+    [CreateAssetMenu(menuName = "Ingredients/Ingredient Item")]
+    public class ScriptableIngredientItem : ScriptableObject
+    {
+        [SerializeField]private IngredientItemBase ingredientPrefab;
+        [SerializeField]private ScriptableIngredientItem[] compatibleIngredients;
+        [SerializeField]private ScriptableIngredientItem[] incompatibleIngredients;
+
+        public IngredientItemBase IngredientPrefab => ingredientPrefab;
+
+        public bool IsCompatibleWith(ScriptableIngredientItem otherIngredient)
+        {
+            if (otherIngredient == null) return false;
+
+            if (System.Array.Exists(incompatibleIngredients, ingredient => ingredient == otherIngredient))
+            {
+                return false; // Eðer uyumsuz yiyecekler arasýnda varsa, uyumsuzdur.
+            }
+
+            return System.Array.Exists(compatibleIngredients, ingredient => ingredient == otherIngredient);
+        }
+
+
+        //public bool IsCompatibleWith(FoodItem otherFood)
+        //{
+        //    if (otherFood == null) { return false; }
+
+        //    return System.Array.Exists(compatibleFoods, food => food == otherFood);
+
+        //    foreach (FoodItem compatible in compatibleFoods)
+        //    {
+        //        if (compatible == otherFood)
+        //        {
+        //            return true;
+        //        }
+        //    }
+        //    return false;
+
+        //}
+
+    }
+
+
+}
