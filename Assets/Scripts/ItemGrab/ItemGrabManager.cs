b890a4b3f1f63dab5f3d35987b63fb8642ee6af2
SemihDurmus1 <58946672+SemihDurmus1@users.noreply.github.com>
2025-02-06 18:06:30 +0300

    *script changes: ItemGrabManager
    New mouse left click input added. Will configure with New Input System
    PrepareSandwich function added

--- Assets/Scripts/ItemGrab/ItemGrabManager.cs
+++ Assets/Scripts/ItemGrab/ItemGrabManager.cs
@@ -25,6 +25,11 @@ namespace Grabbing
             HandleGrabbing();
         }
 
+        private void HandleGrabbing()
+        {
+            PreviewOnSandwichMakerPlane();
+            HandleGrabInput();
+        }
         private void PreviewOnSandwichMakerPlane()// This method previews ingredients on SandwichMakerPlane when the raycast hits
         {
             if (currentIngredient == null) { return; }//return if nothing on hand
@@ -43,46 +48,66 @@ namespace Grabbing
             }
         }
 
-        private void HandleGrabbing()
+        private void HandleGrabInput()//This code is unoptimized and dirty as hell, need to fix it!
         {
-            PreviewOnSandwichMakerPlane();
-            HandleGrabInput();
-        }
-
-        private void HandleGrabInput()
-        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (currentIngredient != null)
+                {
+                    currentIngredient.Drop();
+                    currentIngredient = null;
+                }
+                if (Physics.Raycast(playerCamTransform.position, playerCamTransform.forward,
+                    out RaycastHit raycastHit, pickUpDistance, sandwichMakerPlaneLayerMask))//if ray hits a SandwichMakerPlane
+                {
+                    if (raycastHit.transform.TryGetComponent<SandwichMakerPlane>(out currentSandwichMakerPlane))
+                    {
+                        currentSandwichMakerPlane.PrepareSandwich();
+                    }
+                }    
+            }
             if (inputCenter.IsGrabKeyPressed && !isGrabKeyHolding)//When press the grab key
             {
                 isGrabKeyHolding = true;
-                if (currentIngredient != null)//if we got something on hand
-                {
-                    if (currentSandwichMakerPlane != null)//If we put an ingredient on the sandwich plane
-                    {
-                        PlaceOnSandwichMakerPlane();
-                    }
-                    else
-                    {
-                        currentIngredient.Drop();
-                        currentIngredient = null;
-                    }
-                }
-                else if (Physics.Raycast(playerCamTransform.position, playerCamTransform.forward,
-                         out RaycastHit raycastHit, pickUpDistance, pickUpLayerMask)) //if we got nothing on hand and raycast hits
-                {
-                    if (raycastHit.transform.TryGetComponent<IngredientItem>(out currentIngredient))
-                    {
-                        if (currentIngredient.onThatSandwichMakerPlane != null)//If taken ingredient on a SandwichMakerPlane, remove from there
-                        {
-                            currentIngredient.onThatSandwichMakerPlane.RemoveIngredientFromPlane(currentIngredient);
-                        }
-
-                        currentIngredient.Grab(grabPointTransform);
-                    }
-                }
+                HandleGrabAction();
             }
             else if (!inputCenter.IsGrabKeyPressed)//It allows interact when the key relased
             {
                 isGrabKeyHolding = false;
+            }
+        }
+
+        private void HandleGrabAction()
+        {
+            if (currentIngredient != null)//if we got something on hand
+            {
+                if (currentSandwichMakerPlane != null)//If we put an ingredient on the sandwich plane
+                {
+                    PlaceOnSandwichMakerPlane();
+                }
+                else
+                {
+                    currentIngredient.Drop();
+                    currentIngredient = null;
+                }
+            }
+            else TryPickUpIngredient();
+        }
+
+        private void TryPickUpIngredient()
+        {
+            if (Physics.Raycast(playerCamTransform.position, playerCamTransform.forward,
+                                 out RaycastHit raycastHit, pickUpDistance, pickUpLayerMask)) //if we got nothing on hand and raycast hits
+            {
+                if (raycastHit.transform.TryGetComponent<IngredientItem>(out currentIngredient))
+                {
+                    if (currentIngredient.onThatSandwichMakerPlane != null)//If taken ingredient on a SandwichMakerPlane, remove from there
+                    {
+                        currentIngredient.onThatSandwichMakerPlane.RemoveIngredientFromPlane(currentIngredient);
+                    }
+
+                    currentIngredient.Grab(grabPointTransform);
+                }
             }
         }
 
