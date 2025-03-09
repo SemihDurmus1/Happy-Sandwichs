a8084705104adbf469910f561abc6d3d8d3a23a8
SemihDurmus1 <58946672+SemihDurmus1@users.noreply.github.com>
2025-03-09 20:14:37 +0300

    *script update: OrderComparator no longer Monobehaviour
    -Removed add breads function to orderSandwiches ingredients
    -A few Code cleanings

--- Assets/Scripts/Customer/Order/OrderComparator.cs
+++ Assets/Scripts/Customer/Order/OrderComparator.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 
 namespace Customer.Order
 {
-    public class OrderComparator : MonoBehaviour
+    [System.Serializable]
+    public class OrderComparator
     {
-        private ScriptableIngredientItem bread;
-
-        private void Start()
-        {
-            bread = IngredientCenter.Instance.breadPrefab;
-        }
+        //private ScriptableIngredientItem bread;
+        //private void Start()
+        //{
+        //    //bread = IngredientCenter.Instance.breadPrefab;
+        //}
 
         /// <summary>
         /// Compares two orders to see if they are the same
@@ -22,25 +22,24 @@ namespace Customer.Order
         {
             if (expected == null || delivered == null)//Null check
             {
-                Debug.LogWarning("One of the sandwiches are null.");
-                return false;
+                Debug.LogWarning("One of the sandwiches are null."); return false;
             }
 
-            //If our sandwich's first and last ingredient is bread
-            if (delivered.ingredients[0] == bread && delivered.ingredients[^1] == bread)
-            {
-                //If the first and last ingredient of the expectedIngredient aren't bread
-                if (expected.ingredients[0] != bread && expected.ingredients[^1] != bread)
-                {
-                    expected.ingredients.Insert(0, bread);//Inserts the bread as first item and the array shifts one right
-                    expected.ingredients.Add(bread);
-                }
-            }
-            else
+            //If our sandwich's first or last ingredient isn't bread
+            if (delivered.ingredients[0] != IngredientCenter.Instance.breadItem ||
+                delivered.ingredients[^1]!= IngredientCenter.Instance.breadItem)//^1 == list.Count - 1
             {
                 Debug.Log("Put the breads correct!");//first or last ingredient isnt bread
                 return false;
             }
+
+            ////If the first and last ingredient of the expectedIngredient aren't bread
+            //if (expected.ingredients[0] != IngredientCenter.Instance.breadItem && 
+            //    expected.ingredients[^1]!= IngredientCenter.Instance.breadItem)
+            //{
+            //    expected.ingredients.Insert(0, IngredientCenter.Instance.breadItem);//Inserts the bread as first item and the array shifts one right
+            //    expected.ingredients.Add(IngredientCenter.Instance.breadItem);
+            //}
 
             if (expected.ingredients.Count != delivered.ingredients.Count)//return if lists counts doesn't match
             {
@@ -48,9 +47,7 @@ namespace Customer.Order
                 return false;
             }
 
-            // Sort the ingredients by ID before comparing
-            expected.ingredients = expected.ingredients.OrderBy(ingredient => ingredient.ingredientID).ToList();
-            delivered.ingredients = delivered.ingredients.OrderBy(ingredient => ingredient.ingredientID).ToList();
+            SortLists(expected, delivered);
 
             for (int i = 0; i < expected.ingredients.Count; i++)//Compare Sandwiches
             {
@@ -63,6 +60,17 @@ namespace Customer.Order
             return true;
         }
 
+        /// <summary>
+        /// Sorts the ingredients by ID before comparing OrderSandwiches and DeliveredSandwich
+        /// </summary>
+        /// <param name="expected"></param>
+        /// <param name="delivered"></param>
+        private static void SortLists(SandwichItem expected, SandwichItem delivered)
+        {
+            // Sorts the ingredients by ID before comparing
+            expected.ingredients = expected.ingredients.OrderBy(ingredient => ingredient.ingredientID).ToList();
+            delivered.ingredients = delivered.ingredients.OrderBy(ingredient => ingredient.ingredientID).ToList();
+        }
 
 
 
