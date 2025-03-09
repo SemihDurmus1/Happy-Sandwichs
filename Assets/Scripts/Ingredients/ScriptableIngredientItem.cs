66f44d5ce694ebebbef24083cda8f291d85fee74
SemihDurmus1 <58946672+SemihDurmus1@users.noreply.github.com>
2025-03-09 13:22:13 +0300

    *added ID Enum to ScriptableIngredientItems

--- Assets/Scripts/Ingredients/ScriptableIngredientItem.cs
+++ Assets/Scripts/Ingredients/ScriptableIngredientItem.cs
@@ -2,12 +2,23 @@ using UnityEngine;
 
 namespace Ingredient
 {
+    public enum IngredientID
+    {
+        Bread,
+        Cheddar,
+        Cheese,
+        Egg,
+        Leaf,
+        Meat,
+    }
+
     [CreateAssetMenu(menuName = "Ingredients/Ingredient Item")]
     public class ScriptableIngredientItem : ScriptableObject
     {
-        //Maybe i add ingredientID here
-        [SerializeField]private IngredientItem ingredientPrefab;//saves prefab
+        [SerializeField] public IngredientID ingredientID;
+        //Maybe i add here uniqe ID declare system for ingredients
 
+        [SerializeField] private IngredientItem ingredientPrefab;//saves prefab
         public IngredientItem IngredientPrefab => ingredientPrefab;
 
     }
