c7799838683684d340ac122dafdb1560e8994f14
SemihDurmus1 <58946672+SemihDurmus1@users.noreply.github.com>
2025-04-02 20:39:26 +0300

    *script update: ScriptableIngredientItem.cs
    -Price variable added for creating an wallet system

--- Assets/Scripts/Ingredients/ScriptableIngredientItem.cs
+++ Assets/Scripts/Ingredients/ScriptableIngredientItem.cs
@@ -4,21 +4,16 @@ namespace Ingredient
 {
     public enum IngredientID
     {
-        Bread,
-        Cheddar,
-        Cheese,
-        Egg,
-        Leaf,
-        Meat,
+        Bread, Cheddar, Cheese, Egg, Leaf, Meat
     }
 
     [CreateAssetMenu(menuName = "Ingredients/Ingredient Item")]
     public class ScriptableIngredientItem : ScriptableObject
     {
-        [SerializeField] public IngredientID ingredientID;
-        //Maybe i add here uniqe ID declare system for ingredients
-
         [SerializeField] private IngredientItem ingredientPrefab;//saves prefab
+        public IngredientID ingredientID;
+        public float price;
+
         public IngredientItem IngredientPrefab => ingredientPrefab;
 
     }
