6a0146cc4fe0dc2a54e7f366841308a5bcbaadfd
SemihDurmus1 <58946672+SemihDurmus1@users.noreply.github.com>
2025-04-10 22:50:03 +0300

    *script update: IngredientSlot
    -Buying function now working

--- Assets/Scripts/Market/IngredientSlot.cs
+++ Assets/Scripts/Market/IngredientSlot.cs
@@ -1,18 +1,46 @@
 using Ingredient;
+using Player;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class IngredientSlot : MonoBehaviour
-{
-    public Image icon;
-    public TMP_Text nameText;
-    public TextMeshProUGUI priceText;
-
-    public void SetSlotInfos(ScriptableIngredientItem ingredient)
+namespace Market {
+    public class IngredientSlot : MonoBehaviour
     {
-        icon.sprite = ingredient.ingredientIcon;
-        nameText.text = ingredient.name;
-        priceText.text = ingredient.price.ToString();
+        [SerializeField] private ScriptableIngredientItem _ingredient;
+        public Image icon;
+        public TMP_Text nameText;
+        public TextMeshProUGUI priceText;
+
+        public void SetSlotInfos(ScriptableIngredientItem ingredient)
+        {
+            _ingredient = ingredient;
+            icon.sprite = ingredient.ingredientIcon;
+            nameText.text = ingredient.name;
+            priceText.text = ingredient.price.ToString();
+        }
+
+        public void BuyButtonClicked()
+        {
+            PlayerManager playerManager = FindAnyObjectByType<PlayerManager>();
+
+            bool isEnoughMoney = playerManager.wallet.RemoveFromWallet(_ingredient.buyPrice);
+
+            if (isEnoughMoney)
+            {
+                IngredientDeliverer deliver = FindAnyObjectByType<IngredientDeliverer>();
+                deliver.GetDeliver(_ingredient);
+
+                Debug.Log(nameText.text + " satin alindi");
+            }
+            else
+            {
+                Debug.Log("Amk fakiri");
+            }
+        }
     }
+
+
+
+
 }
