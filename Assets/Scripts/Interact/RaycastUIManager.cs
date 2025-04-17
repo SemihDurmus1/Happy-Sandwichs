fb8fda3742ba0b5faafba567d40f4ae179753a49
SemihDurmus1 <58946672+SemihDurmus1@users.noreply.github.com>
2025-04-17 23:25:45 +0300

    *new script: RaycatUIManager
    -This for handle the screen raycasting interactions

---
+++ Assets/Scripts/Interact/RaycastUIManager.cs
@@ -0,0 +1,62 @@
+using Player;
+using UnityEngine;
+
+public interface IInteractable
+{
+    void ShowInteractUI();
+    void HideInteractUI();
+    void Interact();
+    void DeInteract();
+}
+
+public class RaycastUIManager : MonoBehaviour
+{
+    [SerializeField] private PlayerManager playerManager;
+    public IInteractable currentInteractable;
+    private UIManager uiManager;
+
+    private void Start()
+    {
+        uiManager = GameManager.Instance.uiManager;
+    }
+
+    private void Update()
+    {
+        if (Physics.Raycast(playerManager.camTransform.position, playerManager.camTransform.forward,
+            out RaycastHit raycastHit, playerManager.pickUpDistance, playerManager.interactionLayer))
+        {
+            IInteractable interactable = raycastHit.collider.GetComponent<IInteractable>();
+
+                //if interactable null  or interactable null and current arent null
+            if (interactable == null)
+            {
+                ClearInteraction();
+            }
+            else if (interactable != null && interactable != currentInteractable)//If interactable arent the current
+            {
+                Debug.Log("Baba interactablei buldu");
+                currentInteractable = interactable;
+                interactable.ShowInteractUI();
+            }
+
+            //Interactable varsa ve currentInteractable buysa - bir sey yapma
+            //Interactable varsa ve current bu degilse - currenta ata
+            //Interactable yoksa - Clearla
+            //Interabtable yoksa ama current varsa - Clearla
+        }
+        else
+        {
+            ClearInteraction();
+
+        }
+    }
+
+    private void ClearInteraction()
+    {
+        if (currentInteractable != null)
+        {
+            uiManager.DeactivateUI(uiManager.clickUI);
+            currentInteractable = null;
+        }
+    }
+}
